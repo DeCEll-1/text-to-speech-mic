ecc0c0f419d3215da70f63cc240e05dadef3559d
Arda Eren <Poedx2@gmail.com>
2024-10-23 16:31:08 +0300

    lang & other changes + bat files

--- core (online).py
+++ core (online).py
@@ -1,11 +1,13 @@
-#mAlc_Jke \ vya4es.ru/projects/txt-to-speech-mic
+# mAlc_Jke \ vya4es.ru/projects/txt-to-speech-mic
 
 import gtts
 import pygame
+import os
 
 from time import sleep
 from pydub import AudioSegment
 from pygame import mixer, _sdl2 as devices
+
 
 def play(file_path: str, device: str):
     if device is None:
@@ -21,51 +23,78 @@ def play(file_path: str, device: str):
     sleep(duration)
     pygame.mixer.quit()
 
+
 def text_to_speech(text):
-    tts = gtts.gTTS(text, lang="ru")
-    tts.save('temp.mp3')
+    tts = gtts.gTTS(text, lang="en-us", tld="com.tr")
+    tts.save("temp.mp3")
+
 
 def get_audio_duration(file_path):
     audio = AudioSegment.from_file(file_path)
     duration_in_seconds = len(audio) / 1000  # Преобразование миллисекунд в секунды
     return duration_in_seconds
 
+
 def get_audio_input_device():
     mixer.init()  # Инициализация mixer
-    audio_devices = devices.audio.get_audio_device_names(False)  # Получение всех доступных аудио-входных устройств
+    audio_devices = devices.audio.get_audio_device_names(
+        False
+    )  # Получение всех доступных аудио-входных устройств
 
-    print("Доступные аудио-входные устройства:")
+    print("availableAudioEntryDevices:")
     for i, device in enumerate(audio_devices):
         print(f"{i+1}. {device}")
 
     while True:
         try:
-            choice = int(input("Выберите номер аудио-входного устройства: "))
+            choice = int(input("Select the number of the audio-entry device: "))
             if 1 <= choice <= len(audio_devices):
                 break
             else:
-                print(f"Пожалуйста, выберите номер от 1 до {len(audio_devices)}")
+                print(f"Please select a number from 1 to {len(audio_devices)}")
         except ValueError:
-            print("Пожалуйста, введите корректный номер")
+            print("Please enter the correct number")
 
     selected_device = audio_devices[choice - 1]  # Выбор указанного устройства
-    mixer.quit()  # Завершение работы mixer
+
+    # while True:
+    #     try:
+    #         choice = int(
+    #             input("Select the second device number of the audio-entry device: ")
+    #         )
+    #         if 1 <= choice <= len(audio_devices):
+    #             break
+    #         else:
+    #             print(f"Please select a number from 1 to {len(audio_devices)}")
+    #     except ValueError:
+    #         print("Please enter the correct number")
+
+    # selected_device2 = audio_devices[choice - 1]  # Выбор указанного устройства
+
+    mixer.quit()  # Completion of work mixer
 
     return selected_device
+
 
 def main():
 
     selected_device = get_audio_input_device()
 
     while True:
-        text = input("Введите текст для преобразования в речь (или 'выход' для завершения): ")
-        if text.lower() == 'выход':
+        text = input("")
+        if text.lower() == "выход":
             break
+
+        if text == "":
+            continue
+
+        os.system("cls")
 
         text_to_speech(text)
 
         # Воспроизведение сохраненной речи через виртуальный микрофон
-        play('temp.mp3', selected_device)
+        play("temp.mp3", selected_device)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
---
+++ run.bat
@@ -0,0 +1,1 @@
+python "core (online).py"
